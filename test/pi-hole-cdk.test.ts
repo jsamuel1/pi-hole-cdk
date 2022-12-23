89a07c559287272db5ad520d6c69d9266646cda1
Joshua Samuel <sauhsoj@amazon.com>
2022-12-23 12:13:53 +1100

    Initial commit

---
+++ test/pi-hole-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PiHoleCdk from '../lib/pi-hole-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pi-hole-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PiHoleCdk.PiHoleCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
